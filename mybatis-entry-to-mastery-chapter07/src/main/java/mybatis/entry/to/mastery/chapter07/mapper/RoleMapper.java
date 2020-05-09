0e0b07f431efb3b5df760eb43a291e9449338ab3
balsamspear <balsamspear@163.com>
2020-05-09 17:46:10 +0800

    chapter07，使用阿里云 redis 测试 redis 缓存

--- mybatis-entry-to-mastery-chapter07/src/main/java/mybatis/entry/to/mastery/chapter07/mapper/RoleMapper.java
+++ mybatis-entry-to-mastery-chapter07/src/main/java/mybatis/entry/to/mastery/chapter07/mapper/RoleMapper.java
@@ -6,8 +6,9 @@ import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.mybatis.caches.ehcache.EhcacheCache;
+import org.mybatis.caches.redis.RedisCache;
 
-@CacheNamespace(implementation = EhcacheCache.class)
+@CacheNamespace(implementation = RedisCache.class)
 public interface RoleMapper {
 
     @Select("select id, role_name roleName, enabled, create_by createBy, create_time createTime from sys_role where id = #{id}")
